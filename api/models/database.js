315efd5b605aebdb0a7fe1c48618bea00af8dd41
letrinhan1999 <69832629+letrinhan1999@users.noreply.github.com>
2021-07-03 21:34:59 +0700

    update DB

--- api/models/database.js
+++ api/models/database.js
@@ -4,10 +4,10 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
 var db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'autumn_shop'
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
 })
 
 db.connect(function (err) {  
